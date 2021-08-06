88e1c3b6e76af2a0b97d23037a4da1b00a621ef2
soumyashree <soumyashree.wali@excellarate.com>
2021-08-06 17:46:27 +0530

    javascript pgms added

---
+++ objsmw.js
@@ -0,0 +1,16 @@
+/* Write a JavaScript program to delete the rollno property from the following object. Also print the object before or after deleting the property.
+Sample object:
+var student = {
+name : "David Rayy",
+sclass : "VI",
+rollno : 12 } */
+const student = {
+  name: 'David',
+  sclass: 'IV',
+  rollno: 12
+}
+console.log('student before editing')
+console.log('student name is' + student.name, 'studying in ' + student.sclass, 'with rollno' + student.rollno)
+delete student.rollno
+console.log('student after editing')
+console.log('student name is' + student.name, 'studying in' + student.sclass, 'with rollno' + student.rollno)
